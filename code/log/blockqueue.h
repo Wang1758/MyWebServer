31af959e6e8008bf066ce7124fddc7f191148c8a
Wang1758 <Wang1758@email.com>
2024-11-06 21:29:02 +0800

    修改

---
+++ code/log/blockqueue.h
@@ -0,0 +1,57 @@
+#ifndef BLOCKQUEUE_H
+#define BLOCKQUEUE_H
+
+#include <mutex>
+#include <deque>
+#include <condition_variable>
+#include <sys/time.h>
+
+template<class T>
+class BlockDeque {
+public:
+    explicit BlockDeque(size_t MaxCapacity = 1000);
+
+    ~BlockDeque();
+
+    void clear();
+
+    bool empty();
+
+    bool full();
+
+    void Close();
+
+    size_t size();
+
+    size_t capacity();
+
+    T front();
+
+    T back();
+
+    void push_back(const T &item);
+
+    void push_front(const T &item);
+
+    bool pop(T &item);
+
+    bool pop(T &item, int timeout);
+
+    void flush();
+
+private:
+    std::deque<T> deq_;
+
+    size_t capacity_;
+
+    std::mutex mtx_;
+
+    bool isClose_;
+
+    std::condition_variable condConsumer_;
+
+    std::condition_variable condProducer_;
+};
+
+
+#endif
